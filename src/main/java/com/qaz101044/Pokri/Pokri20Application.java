2e278b6e3548ba4f5cbc3c25b71b705a1241e9d2
KimHongHee <qaz101044@naver.com>
2024-03-06 21:26:35 +0900

    init

---
+++ src/main/java/com/qaz101044/Pokri/Pokri20Application.java
@@ -0,0 +1,13 @@
+package com.qaz101044.Pokri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pokri20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pokri20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qaz101044/Pokri/Pokri20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaz101044.Pokri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pokri20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
